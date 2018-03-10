f44aa45c584e2aceaa1973a86723c93589a8e75a
djxtazy <33640417+djxtazy@users.noreply.github.com>
2018-03-09 21:11:47 -0500

    another change
    another change

--- KQueryTest/UnitTest1.cs
+++ KQueryTest/UnitTest1.cs
@@ -8,64 +8,6 @@ namespace KQueryTest
     public class UnitTest1
     {
         [TestMethod]
-        public void CheckComputerName()
-        {
-            string[] args = { "+CN", "AF-154649" };
-
-            // assert
-            int expected = 0;
-
-            // actual
-            int actual = Program.Main(args);
-
-            Assert.AreEqual(expected, actual);
-
-        }
-
-        public void CheckComputerNameInList()
-        {
-            string[] args = { "+CN", "AF-SDfsdf,AF-154648" };
-
-            // assert
-            int expected = 1;
-
-            // actual
-            int actual = Program.Main(args);
-
-            Assert.AreEqual(expected, actual);
-
-        }
-
-        [TestMethod]
-        public void CheckNotComputerName()
-        {
-            string[] args = { "-CN", "AF-154649" };
-
-            // assert
-            int expected = 1;
-
-            // actual
-            int actual = Program.Main(args);
-
-            Assert.AreEqual(expected, actual);
-
-        }
-
-        public void CheckNotComputerNameList()
-        {
-            string[] args = { "-CN", "AF-234234,AF-154648" };
-
-            // assert
-            int expected = 0;
-
-            // actual
-            int actual = Program.Main(args);
-
-            Assert.AreEqual(expected, actual);
-
-        }
-
-        [TestMethod]
         public void CheckIfX64()
         {
             string[] args = { "/X64" };
